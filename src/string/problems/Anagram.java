f2c7d973895f10a16e10f7b7f65507173d7a6d15
RocksanaTalukder <Rocksanatalukder88@gmail.com>
2019-03-02 22:21:48 -0500

    Anagram

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,4 +1,9 @@
 package string.problems;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+
 
 /**
  * Created by mrahman on 04/22/17.
@@ -6,7 +11,49 @@ package string.problems;
 public class Anagram {
 
     public static void main(String[] args) {
-        //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
-        //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+        char str1[] = { 'c', 'a', 't' };
+        char str2[] = { 'a', 'c', 't' };
+
+        if (areAnagram(str1, str2))
+            System.out.println("The two strings are"
+                    + " anagram of each other");
+        else
+            System.out.println("The two strings are not"
+                    + " anagram of each other");
+    }
+
+    static boolean areAnagram(char[] str1, char[] str2)
+    {
+        int n1 = str1.length;
+        int n2 = str2.length;
+
+        if (n1 != n2)
+            return false;
+
+        // Sorting both strings
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        // Comparing sorted strings
+        for (int i = 0; i < n1; i++)
+            if (str1[i] != str2[i])
+                return false;
+
+        return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
+        //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+
+
